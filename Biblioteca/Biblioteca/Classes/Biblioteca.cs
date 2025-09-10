da7b80549deb21df583a6b327893033fe58cb67d
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-10 11:51:04 -0300

    Add files via upload

---
+++ Biblioteca/Biblioteca/Classes/Biblioteca.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Reflection.Metadata;
+
+namespace Biblioteca.Classes;
+
+public class Biblioteca
+{
+    public List<Livro> livros {  get; set; }
+    public int titulo { get; set; }
+
+
+    public Biblioteca(int Titulo)
+    {
+        titulo = Titulo;
+        {
+            new Livro("Harry Potter", "J. K. Rowling", 1998, true);
+            new Livro("Frankstain", "Mary Shelley", 1818, true);
+
+        }
+    }
+}
+
+
