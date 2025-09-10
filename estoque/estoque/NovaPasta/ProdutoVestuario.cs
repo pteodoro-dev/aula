95d29fbbf1b3d2c462816b832d09b6046242cd71
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-10 10:42:11 -0300

    Add files via upload

---
+++ estoque/estoque/NovaPasta/ProdutoVestuario.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Projeto_Estoque_2.Classes;
+
+public class ProdutoVestuario : Produto
+{
+    public string modelo { get; set; }
+    public string cor { get; set; }
+    public string tamanho { get; set; }
+    public ProdutoVestuario(string Nome, double Preco, int Quantidade, string sku, string Modelo, string Cor, string Tamanho) : base(Nome, Preco, Quantidade, sku)
+    {
+        modelo = Modelo;
+        cor = Cor;
+        tamanho = Tamanho;
+    }
+
+    public override void ExibirDetalhes()
+    {
+        base.ExibirDetalhes();
+        Console.WriteLine($@"| Modelo: {modelo}
+| Cor: {cor}
+| Tamanho: {tamanho}
+|---------------------------------------");
+    }
+}
