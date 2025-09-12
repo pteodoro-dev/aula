fe60163d55e849f2521a0e0a4054eb2c22539385
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-12 11:36:13 -0300

    Add files via upload

---
+++ rpg/rpg/Classes/Monstro.cs
@@ -0,0 +1,10 @@
+﻿public class Monstro : Entidade
+{
+    public string Tipo { get; set; }
+
+    public override void ReceberDano(int dano)
+    {
+        base.ReceberDano(dano);
+        Console.WriteLine($"O monstro {Nome} rugiu de dor! Tipo: {Tipo}");
+    }
+}
---
+++ rpg/rpg/Classes/Personagem.cs
@@ -0,0 +1,11 @@
+﻿public class Personagem : Entidade
+{
+    public string Classe { get; set; }
+
+    public void Curar()
+    {
+        int cura = 20;
+        PontosDeVida += cura;
+        Console.WriteLine($"{Nome} se curou em {cura} pontos. Vida atual: {PontosDeVida}");
+    }
+}
