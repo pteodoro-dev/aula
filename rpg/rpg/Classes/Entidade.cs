a8e183770e5237fcf6b6b73a0175d4d98ff43174
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-15 10:29:49 -0300

    Add files via upload

---
+++ rpg/rpg/Classes/Entidade.cs
@@ -0,0 +1,21 @@
+﻿public abstract class Entidade
+{
+    public string Nome { get; set; }
+    public int PontosDeVida { get; set; }
+    public int Ataque { get; set; }
+
+    public bool EstaVivo => PontosDeVida > 0;
+
+    public virtual void Atacar(Entidade alvo)
+    {
+        Console.WriteLine($"{Nome} ataca {alvo.Nome} causando {Ataque} de dano!");
+        alvo.ReceberDano(Ataque);
+    }
+
+    public virtual void ReceberDano(int dano)
+    {
+        PontosDeVida -= dano;
+        if (PontosDeVida < 0) PontosDeVida = 0;
+        Console.WriteLine($"{Nome} recebeu {dano} de dano. Vida restante: {PontosDeVida}");
+    }
+}
