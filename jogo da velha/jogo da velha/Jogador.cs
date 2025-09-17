93a0874682cb58fc455f2fd3ebb2ed32334d03f3
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-17 09:57:31 -0300

    Add files via upload

---
+++ jogo da velha/jogo da velha/Jogador.cs
@@ -0,0 +1,15 @@
+﻿
+namespace jogo_da_velha;
+
+public class Jogador
+{
+    public string Nome { get; set; }
+
+    public char Simbolo { get; private set; }
+
+    public Jogador(string nome, char simbolo)
+    {
+        Nome = nome;
+        Simbolo = simbolo;
+    }
+}
