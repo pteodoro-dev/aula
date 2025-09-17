08e702335e14a49996070fe89f2bb5fa4dbc1373
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-17 09:45:18 -0300

    Add files via upload

---
+++ jogo da velha/jogo da velha/Program.cs
@@ -0,0 +1,18 @@
+﻿using jogo_da_velha;
+
+Tabuleiro tabuleiro = new Tabuleiro();
+Jogador jogador1 = new Jogador("Jogaodr 1", 'X');
+Jogador jogador2 = new Jogador("Jogador 2", 'O');
+Jogador jogadorAtual = jogador1;
+
+bool jogoTerminou = false;
+
+while (jogoTerminou)
+{
+    tabuleiro.Desenhar();
+
+    Console.WriteLine($"\n{jogadorAtual Nome}, e a sua vez Escolha uma posicao (1-9)");
+    int posicao = int.Parse( Console.ReadLine() );
+
+    
+}
