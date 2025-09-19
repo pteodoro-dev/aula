e4ec205557c8a60f17765637d5ccd6051a2fb61e
pteodoro-dev <p.teodoro@aluno.senai.br>
2025-09-19 11:30:48 -0300

    Add files via upload

---
+++ jogo da velha/jogo da velha/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using jogo_da_velha;
+
+class Program
+{
+    static void Main()
+    {
+        string jogarNovamente;
+
+        do
+        {
+            Tabuleiro tabuleiro = new Tabuleiro();
+            Console.WriteLine("Digite o nome do Jogador 1 (X):");
+            string nomeJogador1 = Console.ReadLine();
+            Jogador jogador1 = new Jogador(nomeJogador1, 'X');
+
+            Console.WriteLine("Digite o nome do Jogador 2 (O):");
+            string nomeJogador2 = Console.ReadLine();
+            Jogador jogador2 = new Jogador(nomeJogador2, 'O');
+
+            Jogador jogadorAtual = jogador1;
+            bool jogoTerminou = false;
+
+            int vitoriasJogador1 = 0;
+            int vitoriasJogador2 = 0;
+
+            while (!jogoTerminou)
+            {
+                tabuleiro.Desenhar();
+
+                Console.WriteLine($"\n{jogador1.Nome} (X): {vitoriasJogador1} vitórias");
+                Console.WriteLine($"{jogador2.Nome} (O): {vitoriasJogador2} vitórias");
+
+                Console.WriteLine($"\n{jogadorAtual.Nome}, é a sua vez. Escolha uma posição (1-9):");
+                int posicao = int.Parse(Console.ReadLine());
+
+                if (tabuleiro.MarcarJogada(posicao, jogadorAtual.Simbolo))
+                {
+                    if (tabuleiro.VerificarVencedor(jogadorAtual.Simbolo))
+                    {
+                        tabuleiro.Desenhar();
+                        Console.WriteLine($"\nParabéns {jogadorAtual.Nome} venceu!");
+
+                        if (jogadorAtual == jogador1)
+                            vitoriasJogador1++;
+                        else
+                            vitoriasJogador2++;
+
+                        jogoTerminou = true;
+                    }
+                    else if (tabuleiro.VerificarEmpate())
+                    {
+                        tabuleiro.Desenhar();
+                        Console.WriteLine("\nO jogo terminou em empate!");
+                        jogoTerminou = true;
+                    }
+                    else
+                    {
+                        jogadorAtual = jogadorAtual == jogador1 ? jogador2 : jogador1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Jogada inválida! Pressione Enter para tentar novamente");
+                    Console.ReadKey();
+                }
+            }
+            Console.WriteLine("\nDesejam jogar novamente? (S/N):");
+            jogarNovamente = Console.ReadLine().ToUpper();
+
+        } while (jogarNovamente == "S");
+
+        Console.WriteLine("Obrigado por jogar!");
+    }
+}
---
+++ jogo da velha/jogo da velha/Tabuleiro.cs
@@ -0,0 +1,70 @@
+﻿
+namespace jogo_da_velha;
+
+public class Tabuleiro
+{
+    private char[] casas;
+
+    public Tabuleiro()
+    {
+        casas = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    }
+
+    public void Desenhar()
+    {
+        Console.Clear();
+        Console.WriteLine($@"{casas[0]} | {casas[1]} | {casas[2]}
+---+---+---
+{casas[3]} | {casas[4]} | {casas[5]}
+---+---+---
+{casas[6]} | {casas[7]} | {casas[8]}");
+    }  
+    
+    public bool MarcarJogada(int posicao, char simbolo)
+    {
+        if(posicao >= 1 && posicao <=  9)
+        {
+            if (casas[posicao - 1] != 'X' && casas[posicao - 1] != 'O')
+            {
+                casas[posicao - 1] = simbolo;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public bool VerificarVencedor(char simbolo)
+    {
+        //horizontal
+        if (casas[0] == simbolo && casas[1] == simbolo && casas[2] == simbolo) { return true; }
+        if (casas[3] == simbolo && casas[4] == simbolo && casas[5] == simbolo) { return true; }
+        if (casas[6] == simbolo && casas[7] == simbolo && casas[8] == simbolo) { return true; }
+
+        //vertical
+        if (casas[0] == simbolo && casas[3] == simbolo && casas[6] == simbolo) { return true; }
+        if (casas[1] == simbolo && casas[4] == simbolo && casas[7] == simbolo) { return true; }
+        if (casas[2] == simbolo && casas[5] == simbolo && casas[8] == simbolo) { return true; }
+
+        //diagonais
+        if (casas[0] == simbolo && casas[4] == simbolo && casas[8] == simbolo) { return true; }
+        if (casas[2] == simbolo && casas[4] == simbolo && casas[6] == simbolo) { return true; }
+
+        return false;
+
+    }
+
+
+    public bool VerificarEmpate()
+    {
+        foreach(var casa in casas)
+        {
+            if (casa != 'X' && casa != '0')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+      
+}
